a4e9ed636f6948b15b406063841fe83822bd741c
Saiteja4041 <saitejadevineni@gmail.com>
2022-06-28 17:35:56 +0000

    added blog feature

---
+++ src/components/UserInfo/index.js
@@ -0,0 +1,16 @@
+// Write your JS code here
+import './index.css'
+
+const UserInfo = () => (
+  <div className="profile-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/profile-img.png"
+      alt="profile"
+      className="profile-picture"
+    />
+    <h1 className="name">Sai Teja Devineni</h1>
+    <p className="profile-info">Software Developer, India</p>
+  </div>
+)
+
+export default UserInfo
